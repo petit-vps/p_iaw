b7aeb5bf0189142214fc37ddcdcc5a914d8b48c6
Petit Vps <vpsgrat8@gmail.com>
2017-03-06 16:56:30 +0300

    test php

--- index.php
+++ index.php
@@ -8,38 +8,7 @@
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    <div class="container-fluid">
-        <!--div class="row">
-            <div class="col-md-3 block1 text-center">
-                <i class="fa fa-home "></i> Home 
-            </div>
-            <div class="col-md-3 block2 text-center">
-                <i class="fa fa-video-camera "></i> Video
-            </div>
-            <div class="col-md-3 block3 text-center">
-                <i class="fa fa-music "></i> Music
-            </div>
-            <div class="col-md-3 block4 text-center ">
-                <i class="fa fa-envelope "></i> Message
-            </div>
-        </div--!>
-        
-        <div class="row">
-            <div class="radio">
-                <label for="">
-                    <input type="radio" name="rad1">femme
-                </label>
-            </div>
-
-            <div>
-                <div class="form-group">
-                    <label for="">tototo</label>
-                    <input type="text" class="form-control" >
-                </div>
-            </div>
-        </div>
-    </div>
-   
+    <?php echo 'toto'?>
     <script src="./assets/js/jquery.min.js"></script>
     <script src="./assets/js/bootstrap.min.js"></script>
 </body>
