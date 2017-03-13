a8c47c445c28aaa48f882ef0ba323f7866193f11
Petit Vps <vpsgrat8@gmail.com>
2017-03-13 16:23:50 +0300

    table multipliacation

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+function mult($m){
+    $res='';
+    for( $i=0; $i<=10; $i++){
+        $res .= '<div class="line text-center">'. $m.'*'.$i.'='.$i*$m.'</div>';
+    }
+    return $res;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +18,18 @@
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    <?php echo 'PHP de merde 5'?>
+    <div class="container">
+    <div class="row">
+    <?php
+for($i=2 ; $i<200 ; $i++){
+    echo '<div class="col-md-2">';
+    echo '<div class="mult"><div class="m_title text-center">Table '.$i.'</div>';
+        echo mult($i);
+        echo '</div></div>';
+    }
+    ?>  
+    </div> 
+    </div>
     <script src="./assets/js/jquery.min.js"></script>
     <script src="./assets/js/bootstrap.min.js"></script>
 </body>
